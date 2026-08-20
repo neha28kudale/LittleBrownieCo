3874ac41cba1a73550397a24fad375af0fe2a1c5
Neha Kudale <nehakudale1234@gmail.com>
2026-08-20 22:36:57 +0530

    Update index.tsx

--- src/routes/_site/index.tsx
+++ src/routes/_site/index.tsx
@@ -341,7 +341,7 @@ function Home() {
         if (el) el.scrollTo({ left: next * el.clientWidth, behavior: "smooth" });
         return next;
       });
-    }, 4000);
+    }, 2000);
     return () => clearInterval(interval);
   }, []);
 
