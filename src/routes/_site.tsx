77e64d7d9c0e59ffc85b252ae5a54a696553adbf
Neha Kudale <nehakudale1234@gmail.com>
2026-08-23 20:54:03 +0530

    Update _site.tsx

--- src/routes/_site.tsx
+++ src/routes/_site.tsx
@@ -1,8 +1,9 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useLocation } from "@tanstack/react-router";
 import { Header } from "@/components/site/Header";
 import { Footer } from "@/components/site/Footer";
 import { WhatsAppFab } from "@/components/site/WhatsAppFab";
 import { AllergyBanner } from "@/components/site/AllergyBanner";
+import { ScrollProgressBar } from "@/components/site/ScrollProgressBar";
 import { CartProvider } from "@/lib/cart";
 import { FavoritesProvider } from "@/lib/favorites";
 import { Toaster } from "sonner";
@@ -12,14 +13,20 @@ export const Route = createFileRoute("/_site")({
 });
 
 function SiteLayout() {
+  const location = useLocation();
+
   return (
     <FavoritesProvider>
       <CartProvider>
         <div className="flex min-h-screen flex-col">
+          <ScrollProgressBar />
           <Header />
           <AllergyBanner />
           <main className="flex-1">
-            <Outlet />
+            {/* key={pathname} forces a remount on route change, which retriggers animate-page-in */}
+            <div key={location.pathname} className="animate-page-in">
+              <Outlet />
+            </div>
           </main>
           <Footer />
           <WhatsAppFab />
