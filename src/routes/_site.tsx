2fcc2dbb5dc71c6c852ffa9f373a41a8b6c77936
Neha Kudale <nehakudale1234@gmail.com>
2026-08-24 10:06:17 +0530

    Update _site.tsx

--- src/routes/_site.tsx
+++ src/routes/_site.tsx
@@ -4,6 +4,7 @@ import { Footer } from "@/components/site/Footer";
 import { WhatsAppFab } from "@/components/site/WhatsAppFab";
 import { AllergyBanner } from "@/components/site/AllergyBanner";
 import { ScrollProgressBar } from "@/components/site/ScrollProgressBar";
+import { CartDrawer } from "@/components/site/CartDrawer";
 import { CartProvider } from "@/lib/cart";
 import { FavoritesProvider } from "@/lib/favorites";
 import { Toaster } from "sonner";
@@ -30,6 +31,7 @@ function SiteLayout() {
           </main>
           <Footer />
           <WhatsAppFab />
+          <CartDrawer />
           <Toaster position="bottom-center" theme="light" />
         </div>
       </CartProvider>
